ca2b1cd10c704cf420956b71559813ccff255238
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2020-10-12 08:58:02 +0700

    Chapter 7: Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,36 @@
 class UsersController < ApplicationController
-  def new; end
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t "message.user.cant_find_user"
+      @users = User.all
+      render "index"
+    else
+      flash[:success] = t "message.welcome_message"
+    end
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "message.welcome_message"
+      redirect_to @user
+    else
+      flash.now[:danger] = t "message.user.fail"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit User::USER_PARAMS
+  end
 end
