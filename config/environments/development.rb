79ef4fa939ee15c7dd9c3b4c84278045570b0f3a
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2020-10-15 10:55:41 +0700

    Chapter 11: Account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,9 +21,20 @@ Rails.application.configure do
 
   config.active_storage.service = :local
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["USER_EMAIL"],
+    password: ENV["USER_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   config.active_support.deprecation = :log
 
